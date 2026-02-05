df015d0bef221e25067b527f2669ee18c5eed20b
Jeongmin <seomin0610@naver.com>
2026-02-05 20:53:02 +0900

    AndroidBoard.mk에 prebuilt dtbo.img를 dtbo.img 로 복사하는 규칙 추가

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,3 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+DTBO_PREBUILT := $(LOCAL_PATH)/prebuilt/dtbo.img
+DTBO_OBJ := $(PRODUCT_OUT)/obj/DTBO_OBJ/arch/arm64/boot/dtbo.img
+
+$(DTBO_OBJ): $(DTBO_PREBUILT)
+	@echo "Copying prebuilt dtbo.img to DTBO_OBJ"
+	@mkdir -p $(dir $@)
+	@cp -f $< $@
+
 include $(CLEAR_VARS)
