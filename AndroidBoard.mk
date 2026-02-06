234e0d61bf929ea0010aff5bbf961d2be583bfab
Jeongmin <seomin0610@naver.com>
2026-02-06 14:05:01 +0900

    수정2

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -8,4 +8,12 @@ $(DTBO_OBJ): $(DTBO_PREBUILT)
 	@mkdir -p $(dir $@)
 	@cp -f $< $@
 
+DTB_PREBUILT := $(LOCAL_PATH)/prebuilt/dtb.img
+DTB_IMG := $(PRODUCT_OUT)/dtb.img
+
+$(DTB_IMG): $(DTB_PREBUILT)
+	@echo "Copying prebuilt dtb.img to product out"
+	@mkdir -p $(dir $@)
+	@cp -f $< $@
+
 include $(CLEAR_VARS)
