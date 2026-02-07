05fcb27afacdf9e4829cd71472b9b0c9a8c77d07
Jeongmin <seomin0610@naver.com>
2026-02-07 15:23:43 +0900

    수정

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,17 +37,19 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno618
 
 # Kernel
 BOARD_KERNEL_CMDLINE := \
+            console=ttyMSM0,115200n8 \
             androidboot.hardware=qcom \
+            androidboot.console=ttyMSM0 \
             androidboot.memcg=1 \
             lpm_levels.sleep_disabled=1 \
+            video=vfb:640x400,bpp=32,memsize=3072000 \
             msm_rtb.filter=0x237 \
             service_locator.enable=1 \
             swiotlb=1 \
             androidboot.usbcontroller=a600000.dwc3 \
+            earlycon=msm_geni_serial,0x880000 \
             loop.max_part=7 \
-            cgroup.memory=nokmem,nosocket \
-            androidboot.boot_devices=soc/1d84000.ufshc \
-            buildvariant=eng
+            buildvariant=user
 
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_INCLUDE_DTB_IN_BOOTIMG := false
@@ -61,7 +63,7 @@ BOARD_KERNEL_HEADER_VERSION := 2
 BOARD_KERNEL_BASE                 := 0x00000000
 BOARD_KERNEL_TAGS_OFFSET       := 0x00000100
 BOARD_KERNEL_OFFSET              := 0x00008000
-BOARD_KERNEL_SECOND_OFFSET    := 0x00f00000
+BOARD_KERNEL_SECOND_OFFSET    := 0x00000000
 BOARD_RAMDISK_OFFSET             := 0x01000000
 BOARD_DTB_OFFSET                  := 0x01f00000
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
